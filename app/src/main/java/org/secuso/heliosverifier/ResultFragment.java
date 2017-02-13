b4dd96f87a6da02ec41760b9d92c3679cc7baafa
Karola Marky <yonjuni@pinyto.de>
2017-02-13 20:15:25 +0100

    Buttons for success and alarm added

--- app/src/main/java/org/secuso/heliosverifier/ResultFragment.java
+++ app/src/main/java/org/secuso/heliosverifier/ResultFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by yonjuni on 13.02.17.
@@ -17,7 +18,42 @@ public class ResultFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_result, container, false);
         container.removeAllViews();
 
+        Bundle extras = this.getArguments();
+        String candidate = extras.getString("code");
+
+        TextView candidateTextView = (TextView) rootView.findViewById(R.id.resultTextView);
+        candidateTextView.setText(processResult(candidate));
+
         return rootView;
     }
+
+    protected String processResult(String result) {
+
+        switch (result) {
+            case "01":
+                return "Candidate 1";
+            case "02":
+                return "Candidate 2";
+            case "03":
+                return "Candidate 3";
+            case "04":
+                return "Candidate 4";
+            case "05":
+                return "Candidate 5";
+            case "06":
+                return "Candidate 6";
+            case "07":
+                return "Candidate 7";
+            case "08":
+                return "Candidate 8";
+            case "09":
+                return "Candidate 9";
+            case "10":
+                return "Candidate 10";
+            default:
+                return "Empty Vote";
+        }
+    }
+
 
 }
