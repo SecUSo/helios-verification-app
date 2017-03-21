65318d0e4cd2590abfa49bc0258a3c34271344aa
yonjuni <karola.marky@secuso.org>
2017-03-21 03:49:30 +0100

    Wrong QR-Code Hint

--- app/src/main/java/org/secuso/heliosverifier/FirstInstructionFragment.java
+++ app/src/main/java/org/secuso/heliosverifier/FirstInstructionFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
@@ -44,6 +45,8 @@ public class FirstInstructionFragment extends Fragment {
             } else {
                 if (result.getContents().length() > 53) {
                     Toast.makeText(getActivity(), getString(R.string.first_wrong_qr_code), Toast.LENGTH_LONG).show();
+                    final TextView wrong_qr = (TextView) getActivity().findViewById(R.id.wrong_qr);
+                    wrong_qr.setVisibility(View.VISIBLE);
                 } else {
                     openNewFragment();
                 }
